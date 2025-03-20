d6f5e788b5fa9c01b4b6da2a755345518333e163
mehdifeshki <mahdifeshki08@gmail.com>
2025-03-21 01:00:02 +0330

    Update p2.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    for(int i=7; i>3 ; i--){
-        cout <<"i:" << i <<endl;
-        for(int j=0; j<=4 ; j++){
-            cout <<"j:" << j <<endl;
-        }
+
+class Individual {
+private:
+    string firstName, lastName;
+    int yearsOld;
+
+public:
+    Individual() : firstName(""), lastName(""), yearsOld(0) {}
+
+    void setDetails(const string& fn, const string& ln, int age) {
+        firstName = fn;
+        lastName = ln;
+        yearsOld = age;
     }
+
+    int retrieveAge() const {
+        return yearsOld;
+    }
+
+    void showDetails() const {
+        cout << "Full Name: " << firstName << " " << lastName << " | Age: " << yearsOld << endl;
+    }
+};
+
+int main() {
+    const int peopleCount = 10;
+    Individual group[peopleCount];
+
+    for (int i = 0; i < peopleCount; ++i) {
+        string firstName, lastName;
+        int age;
+
+        cout << "Provide data for individual " << i + 1 << ":\n";
+        cout << "First Name: ";
+        cin >> firstName;
+        cout << "Last Name: ";
+        cin >> lastName;
+        cout << "Age: ";
+        cin >> age;
+
+        group[i].setDetails(firstName, lastName, age);
+    }
+
+    int sumAges = 0;
+    for (int i = 0; i < peopleCount; ++i) {
+        sumAges += group[i].retrieveAge();
+    }
+    double avgAge = static_cast<double>(sumAges) / peopleCount;
+    cout << "Average age among all individuals: " << avgAge << endl;
+
+    return 0;
 }
