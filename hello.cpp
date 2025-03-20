c179beb7e2ac39bd3454ea0c544ab318ac422304
mehdifeshki <mahdifeshki08@gmail.com>
2025-03-21 00:58:14 +0330

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=7; i>3 ; i--){
+        cout <<"i:" << i <<endl;
+        for(int j=0; j<=4 ; j++){
+            cout <<"j:" << j <<endl;
+        }
+    }
+}
